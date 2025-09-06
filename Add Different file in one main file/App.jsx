50978aa34b44eb4107004ff5451b227ae5761c77
Sumaya Afrin Era <afrinera432@gmail.com>
2025-09-06 21:46:29 +0600

    Add files via upload

---
+++ Add Different file in one main file/App.jsx
@@ -0,0 +1,14 @@
+ import React from 'react';
+ import demo from './demo';
+ 
+ 
+ const App = () => {
+  return (
+    <div>
+       <h1>Hello Everyone........</h1>
+      <demo/>
+    </div>
+  );
+ };
+ 
+ export default App;
